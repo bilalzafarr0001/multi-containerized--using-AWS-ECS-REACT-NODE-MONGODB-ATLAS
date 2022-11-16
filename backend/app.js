e5d82228e0239c24744a3339b9fc43409e19c60c
Muhammad Bilal <muhammadbilal@nxb.com.pk>
2022-11-16 12:42:43 +0500

    Update backend file Dockerfile and etc

--- backend/app.js
+++ backend/app.js
@@ -1,25 +1,27 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+require("dotenv").config();
 
-const Goal = require('./models/goal');
+const Goal = require("./models/goal");
 
 const app = express();
 
 app.use(bodyParser.json());
+const uri = process.env.ATLAS_URI;
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   next();
 });
 
-app.get('/goals', async (req, res) => {
-  console.log('TRYING TO FETCH GOALS!');
+app.get("/goals", async (req, res) => {
+  console.log("TRYING TO FETCH GOALS");
   try {
     const goals = await Goal.find();
     res.status(200).json({
@@ -28,21 +30,21 @@ app.get('/goals', async (req, res) => {
         text: goal.text,
       })),
     });
-    console.log('FETCHED GOALS');
+    console.log("FETCHED GOALS");
   } catch (err) {
-    console.error('ERROR FETCHING GOALS');
+    console.error("ERROR FETCHING GOALS");
     console.error(err.message);
-    res.status(500).json({ message: 'Failed to load goals.' });
+    res.status(500).json({ message: "Failed to load goals." });
   }
 });
 
-app.post('/goals', async (req, res) => {
-  console.log('TRYING TO STORE GOAL');
+app.post("/goals", async (req, res) => {
+  console.log("TRYING TO STORE GOAL");
   const goalText = req.body.text;
 
   if (!goalText || goalText.trim().length === 0) {
-    console.log('INVALID INPUT - NO TEXT');
-    return res.status(422).json({ message: 'Invalid goal text.' });
+    console.log("INVALID INPUT - NO TEXT");
+    return res.status(422).json({ message: "Invalid goal text." });
   }
 
   const goal = new Goal({
@@ -53,41 +55,50 @@ app.post('/goals', async (req, res) => {
     await goal.save();
     res
       .status(201)
-      .json({ message: 'Goal saved', goal: { id: goal.id, text: goalText } });
-    console.log('STORED NEW GOAL');
+      .json({ message: "Goal saved", goal: { id: goal.id, text: goalText } });
+    console.log("STORED NEW GOAL");
   } catch (err) {
-    console.error('ERROR FETCHING GOALS');
+    console.error("ERROR FETCHING GOALS");
     console.error(err.message);
-    res.status(500).json({ message: 'Failed to save goal.' });
+    res.status(500).json({ message: "Failed to save goal." });
   }
 });
 
-app.delete('/goals/:id', async (req, res) => {
-  console.log('TRYING TO DELETE GOAL');
+app.delete("/goals/:id", async (req, res) => {
+  console.log("TRYING TO DELETE GOAL");
   try {
     await Goal.deleteOne({ _id: req.params.id });
-    res.status(200).json({ message: 'Deleted goal!' });
-    console.log('DELETED GOAL');
+    res.status(200).json({ message: "Deleted goal!" });
+    console.log("DELETED GOAL");
   } catch (err) {
-    console.error('ERROR FETCHING GOALS');
+    console.error("ERROR FETCHING GOALS");
     console.error(err.message);
-    res.status(500).json({ message: 'Failed to delete goal.' });
+    res.status(500).json({ message: "Failed to delete goal." });
   }
 });
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
+const connection = mongoose.connection;
 
-mongoose.connect(
-  `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_URL}/${process.env.MONGODB_NAME}?retryWrites=true&w=majority`,
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  },
-  (err) => {
-    if (err) {
-      console.error('FAILED TO CONNECT TO MONGODB');
-      console.error(err);
-    } else {
-      console.log('CONNECTED TO MONGODB!!');
-      app.listen(80);
-    }
-  }
-);
+connection.once("open", () => {
+  console.log("Connected Database Successfully");
+});
+
+app.listen(80, function (req, res) {
+  console.log("Server is started on port 80");
+});
+// mongoose.connect(
+//   uri,
+//   {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   },
+//   (err) => {
+//     if (err) {
+//       console.error("FAILED TO CONNECT TO MONGODB");
+//       console.error(err);
+//     } else {
+//       console.log("CONNECTED TO MONGODB!!");
+//       app.listen(80);
+//     }
+//   }
+// );
